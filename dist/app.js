20af278ee6743c3db6134cdf79b79c058369b85e
Ranjit Kumar <ranjitkumar448@yahoo.com>
2022-01-31 14:29:32 +0530

    changed root route

--- dist/app.js
+++ dist/app.js
@@ -12,13 +12,6 @@ const index_1 = __importDefault(require("./lib/middleware/index"));
 const routes_1 = __importDefault(require("./routes"));
 const errorHandlers_middleware_1 = __importDefault(require("./lib/middleware/errorHandlers.middleware"));
 const dbConnection_1 = __importDefault(require("./lib/helpers/dbConnection"));
-// import { config } from "process";
-/* Importing defaults */
-// import "./lib/services/cache";
-// Uncomment this when you are ready to use cache.
-/* Error Handlers */
-// These error handlers will caught any unhandled exceptions or rejections
-// and do not stop the process with zero.
 process.on("uncaughtException", e => {
     console.log(e);
     console.log(e.message, "uncaught");
@@ -28,33 +21,6 @@ process.on("unhandledRejection", e => {
     console.log(e, "unhandled");
     process.exit(1);
 });
-// import csv from "csv-parser";
-// import fs from "fs"
-// import questionModel from "./components/question/question.model";
-// const results:any = [];
-//   fs.createReadStream("D:\\Quiz_data\\World_Organizations.csv")
-//     .pipe(csv())
-//     .on('data', (data: any) => {
-//       const questionBody = {
-//         'answer': data.answer_num,
-//         'points': 10,
-//         'isText': 'true',
-//         'categoryId': '6175763454df7246bc6e053c',
-//         'question': data.question,
-//         'level': 2,
-//         'options': [{ "isText": "true", "text": data.option1.split('.')[1] },
-//         { "isText": "true", "text": data.option2.split('.')[1] },
-//         { "isText": "true", "text": data.option3.split('.')[1] },
-//         { "isText": "true", "text": data.option4.split('.')[1] }]
-//       }
-//       results.push(questionBody);
-//     })
-//     .on('end', () => {
-//       results.forEach(async (data: any) => {
-//         await questionModel.create(data, '6155b8be8bc31d20609c4ce9')
-//       });
-//       console.log("end");
-//     });
 // Initialize express app
 const app = express_1.default();
 exports.app = app;
@@ -67,17 +33,10 @@ dbConnection_1.default.mongoConnection();
 |---------------------------------------*/
 // Different router required to initialize different apis call.
 const r1 = express_1.default.Router();
-// for recurrent jobs
-// schedule('* * * * *', async () => {
-//   console.log("Running job......");
-// });
-// schedule('* * * * *', async () => {
-//   console.log("Running job 2......");
-// });
-app.use("/multi", utils_1.applyRoutes(routes_1.default, r1)); // default api
+app.use("/", utils_1.applyRoutes(routes_1.default, r1)); // default api
 app.all("*", (req, res, next) => {
     // res.send("hii");
-    next(new httpErrors_1.HTTP400Error(`Can't found ${req.originalUrl} on  server`));
+    next(new httpErrors_1.HTTP400Error(`Can't found ${req.originalUrl} on services server`));
 });
 console.log("current env is ", process.env.NODE_ENV);
 /*---------------------------------------git push -u myorigin
--- src/app.ts
+++ src/app.ts
@@ -8,14 +8,7 @@ import errorHandlersMiddleware from "./lib/middleware/errorHandlers.middleware";
 import dbConnection from "./lib/helpers/dbConnection";
 
 import { schedule } from 'node-cron';
-// import { config } from "process";
-/* Importing defaults */
-// import "./lib/services/cache";
-// Uncomment this when you are ready to use cache.
 
-/* Error Handlers */
-// These error handlers will caught any unhandled exceptions or rejections
-// and do not stop the process with zero.
 process.on("uncaughtException", e => {
     console.log(e)
   console.log(e.message, "uncaught");
@@ -27,35 +20,6 @@ process.on("unhandledRejection", e => {
   process.exit(1);
 });
 
-// import csv from "csv-parser";
-// import fs from "fs"
-// import questionModel from "./components/question/question.model";
-// const results:any = [];
-
-//   fs.createReadStream("D:\\Quiz_data\\World_Organizations.csv")
-//     .pipe(csv())
-//     .on('data', (data: any) => {
-//       const questionBody = {
-//         'answer': data.answer_num,
-//         'points': 10,
-//         'isText': 'true',
-//         'categoryId': '6175763454df7246bc6e053c',
-//         'question': data.question,
-//         'level': 2,
-//         'options': [{ "isText": "true", "text": data.option1.split('.')[1] },
-//         { "isText": "true", "text": data.option2.split('.')[1] },
-//         { "isText": "true", "text": data.option3.split('.')[1] },
-//         { "isText": "true", "text": data.option4.split('.')[1] }]
-//       }
-
-//       results.push(questionBody);
-//     })
-//     .on('end', () => {
-//       results.forEach(async (data: any) => {
-//         await questionModel.create(data, '6155b8be8bc31d20609c4ce9')
-//       });
-//       console.log("end");
-//     });
 
 // Initialize express app
 const app: express.Application = express();
@@ -73,19 +37,10 @@ dbConnection.mongoConnection();
 // Different router required to initialize different apis call.
 const r1 = express.Router();
 
-// for recurrent jobs
-// schedule('* * * * *', async () => {
-//   console.log("Running job......");
-// });
-
-// schedule('* * * * *', async () => {
-//   console.log("Running job 2......");
-// });
-
-app.use("/multi", applyRoutes(routes, r1)); // default api
+app.use("/", applyRoutes(routes, r1)); // default api
 app.all("*", (req, res, next) => {
   // res.send("hii");
-  next(new HTTP400Error(`Can't found ${req.originalUrl} on  server`));
+  next(new HTTP400Error(`Can't found ${req.originalUrl} on services server`));
 });
 console.log("current env is ",process.env.NODE_ENV );
 
