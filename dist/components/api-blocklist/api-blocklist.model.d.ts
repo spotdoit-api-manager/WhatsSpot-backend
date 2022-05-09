3dd7dd77fd613b4470d73ad1a196d470a644ce52
Ranjit Kumar <ranjitkumar448@yahoo.com>
2022-05-09 19:12:37 +0530

    removed dist file

--- dist/components/api-blocklist/api-blocklist.model.d.ts
+++
@@ -1,6 +0,0 @@
-import { IApiKeyModal } from "../device/device.shema";
-export declare class ApiBlockListModel {
-    addApiToBlockList(deviceId: string, apiDetails: IApiKeyModal): Promise<void>;
-}
-declare const _default: ApiBlockListModel;
-export default _default;
--- dist/lib/services/schedular.d.ts
+++
@@ -1,6 +0,0 @@
-import { IApiKeyModal } from "../../components/device/device.shema";
-export declare class SpotSchedular {
-    scheduleApiExpiration(deviceId: string, apiKeyData: IApiKeyModal): Promise<void>;
-}
-declare const _default: SpotSchedular;
-export default _default;
