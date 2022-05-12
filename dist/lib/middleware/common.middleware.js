17ed497e9f2227bffcf3b2edc5f78588b5ccbffc
Ranjit Kumar <ranjitkumar448@yahoo.com>
2022-05-12 09:53:20 +0530

    latest compiled

--- dist/lib/middleware/common.middleware.js
+++ dist/lib/middleware/common.middleware.js
@@ -3,32 +3,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleCompression = exports.reqConsoleLogger = exports.handleBodyRequestParsing = exports.useHelmet = void 0;
+exports.handleCompression = exports.reqConsoleLogger = exports.handleBodyRequestParsing = exports.allowCors = exports.useHelmet = void 0;
 const express_1 = require("express");
+const cors_1 = __importDefault(require("cors"));
 const compression_1 = __importDefault(require("compression"));
 const helmet_1 = __importDefault(require("helmet"));
+/* Custom imports */
+const config_1 = require("../../config");
 const requestLogger_1 = require("./requestLogger");
 exports.useHelmet = (router) => {
     router.use(helmet_1.default());
 };
-// export const allowCors = (router: Router) => {
-//   router.use(cors({
-//     origin(origin, callback) {
-//       // if (!origin) {
-//       //   return callback(null, true);
-//       // }
-//       console.log(`Origin: ${origin}`,router);
-//       if (configCors.allowOrigin.indexOf(origin) === -1) {
-//         const msg = "The CORS policy for this site does not allow access from the specified Origin.";
-//         return callback(new Error(msg), false);
-//       }
-//       return callback(null, true);
-//     },
-//     exposedHeaders: configCors.exposedHeaders,
-//     // To enable HTTP cookies over CORS
-//     // credentials: true,
-//   }));
-// };
+exports.allowCors = (router) => {
+    router.use(cors_1.default({
+        origin(origin, callback) {
+            if (!origin) {
+            }
+            return callback(null, true);
+            console.log(`Origin: ${origin}`, router);
+            if (config_1.configCors.allowOrigin.indexOf(origin) === -1) {
+                const msg = "The CORS policy for this site does not allow access from the specified Origin.";
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        },
+        exposedHeaders: config_1.configCors.exposedHeaders,
+    }));
+};
 /* here all middleware come. Don't need to do anything in app.js*/
 exports.handleBodyRequestParsing = (router) => {
     router.use(express_1.urlencoded({ extended: true }));
